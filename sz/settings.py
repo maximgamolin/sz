f0c5bd645248ac7c224a2333f87868aa807f4af4
Maxim Gamolin <maksimgamolin@gmail.com>
2023-02-19 12:29:12 +0400

    Добавил Авторизацию

--- sz/settings.py
+++ sz/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'idea'
+    "accounts",
+    'idea',
 ]
 
 MIDDLEWARE = [
@@ -126,3 +127,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = ""
+LOGOUT_REDIRECT_URL = ""
+AUTH_USER_MODEL = "accounts.CustomUser"
--- sz/urls.py
+++ sz/urls.py
@@ -20,5 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('idea.urls'))
+    path('', include('idea.urls')),
+    path("accounts/", include("accounts.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
